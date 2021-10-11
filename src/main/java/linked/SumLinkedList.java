8f7d4c3df7560ec5c7e80204321fa3f8ec8844e7
iranna.patil <iranna.patil@irdeto.con>
2021-10-11 22:15:16 +0200

    run

--- src/main/java/linked/SumLinkedList.java
+++ src/main/java/linked/SumLinkedList.java
@@ -6,4 +6,5 @@ where each node contains a sin- gle digit The digits are stored in reverse order
 such that the 1’s digit is at the head of the list Write a function that adds the two numbers and returns the sum as a linked list
  */
 public class SumLinkedList {
+
 }
