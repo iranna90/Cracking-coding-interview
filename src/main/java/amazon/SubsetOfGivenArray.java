67431f76a9ed21516de18d508b2e34dd8b848b9c
iranna <iranna90@gmail.com>
2021-10-22 00:11:35 +0200

    All subsets.

---
+++ src/main/java/amazon/SubsetOfGivenArray.java
@@ -0,0 +1,23 @@
+package amazon;
+
+/*
+Find all subsets of a given set of integers
+We are given a set of integers and we have to find all the possible subsets of this set of integers. The following example elaborates on this further.
+
+Given set of integers:
+
+2
+3
+4
+All possile subsets for the given set of integers:
+
+2
+3
+2, 3
+4
+2, 4
+3, 4
+2, 3, 4
+ */
+public class SubsetOfGivenArray {
+}
